8d93695f9efa9e01eff7458bbe0523f8d1fe3de9
scottishpalms <119752112+scottishpalms@users.noreply.github.com>
2022-12-03 12:10:47 -0500

    Update day3.py
    made into one loop

--- day3.py
+++ day3.py
@@ -1,7 +1,9 @@
 ASCII_UPPER_OFFSET = 38
 ASCII_LOWER_OFFSET = 96
 f = open("input.txt", "r")
-answer = 0
+answer1 = 0
+answer2 = 0
+i=0
 for line in f:
     length = len(line)
     length = (int)((length-1)/2)
@@ -10,33 +12,26 @@ for line in f:
     for letter in ruck1:
         for comp in ruck2:
             if letter == comp:
-                match = letter
-    if match.isupper():
-        answer += ord(match) - ASCII_UPPER_OFFSET
-    else: answer += ord(match) - ASCII_LOWER_OFFSET
-print("Part 1 Answer: ",answer)
-i=0
-elf1 = "x"
-elf2 = "x"
-elf3 = "x"
-answer = 0
-f.seek(0)
-for line2 in f:
-    line2 = line2.strip()
+                match1 = letter
+    if match1.isupper():
+        answer1 += ord(match1) - ASCII_UPPER_OFFSET
+    else: answer1 += ord(match1) - ASCII_LOWER_OFFSET
+    #start part 2:
+    line = line.strip()
     if i%3 == 0:
-        elf1 = line2
+        elf1 = line
     elif (i%3 == 1):
-        elf2 = line2       
+        elf2 = line       
     else:
-        elf3 = line2
-        #print(elf1, elf2, elf3)
+        elf3 = line
         for letter1 in elf1:
             for letter2 in elf2:
                 for letter3 in elf3:
                     if(letter1 == letter2 and letter2 == letter3):
-                        match = letter1 
-        if match.isupper():
-            answer += ord(match) - ASCII_UPPER_OFFSET
-        else: answer += ord(match) - ASCII_LOWER_OFFSET
+                        match2 = letter1 
+        if match2.isupper():
+            answer2 += ord(match2) - ASCII_UPPER_OFFSET
+        else: answer2 += ord(match2) - ASCII_LOWER_OFFSET
     i+=1
-print("Part 2 Answer: ", answer)
+print("Part 1 Answer: ", answer1)
+print("Part 2 Answer: ", answer2)
