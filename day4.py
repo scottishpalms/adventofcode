632999cc30a52da33f5e011ef90f2174a65efb83
scottishpalms <119752112+scottishpalms@users.noreply.github.com>
2022-12-04 16:32:30 -0500

    simplified part 2

--- day4.py
+++ day4.py
@@ -12,7 +12,7 @@ for line in f:
 
     if (elf1[0]>=elf2[0] and elf1[1]<=elf2[1]) or (elf2[0]>=elf1[0] and elf2[1]<=elf1[1]):
         answer1+=1        
-    if (elf1[0]>=elf2[0] and elf1[0]<=elf2[1]) or (elf1[1]>=elf2[0] and elf1[0]<=elf2[1]) or (elf2[0]>=elf1[0] and elf2[0]<=elf1[1]) or (elf2[1]>=elf1[0] and elf2[1]<=elf1[1]):
+    if (elf1[0]>=elf2[0] and elf1[0]<=elf2[1]) or (elf2[0]>=elf1[0] and elf2[0]<=elf1[1]):
         answer2+=1    
 print("Part 1 answer: ", answer1)
 print("Part 2 answer: ", answer2)
