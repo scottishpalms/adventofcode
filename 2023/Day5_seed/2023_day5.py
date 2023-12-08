2368bc225b97012b3d593198159ed3ca55f3bf38
dc <dwc763@gmail.com>
2023-12-08 00:33:09 -0500

    day 7 done(only computes part 2 though)

---
+++ 2023/Day5_seed/2023_day5.py
@@ -0,0 +1,131 @@
+import math
+
+def getAnswer1(data, seeds):
+    for j, round in enumerate(data):
+        #print(round)
+        for i, seed in enumerate(seeds):
+            for line in round: 
+                #print("start value =", seed)
+                if(seed>=line[1] and seed<(line[1]+line[2])):
+                    seeds[i] = line[0]+(seed-line[1])
+                    #print ("mapped value:",seeds[i], " destination range")
+    return min(seeds)
+
+
+def getAnswer(data, seed):
+    for j, round in enumerate(data):
+        #print(round)
+            for line in round: 
+                #print("start value =", seed)
+                if(seed>=line[1] and seed<(line[1]+line[2])):
+                    seed = line[0]+(seed-line[1])
+                    #print ("mapped value:",seeds[i], " destination range")
+    return seed
+def computeStep(start, length, data, location):
+    startAnswer = getAnswer(data, start)
+    if length == 0:
+        return startAnswer
+    endAnswer = getAnswer(data, (start+length))
+    #print(length)
+    answer2 = min(startAnswer, endAnswer)
+    #print(startAnswer, endAnswer)
+    inSameGroup = False
+    for locale in location:
+        if(((startAnswer in range(locale[0], locale[0]+locale[2]+1)) and (endAnswer in range(locale[0], locale[0]+locale[2]+1)))):
+            inSameGroup = True
+            #print("Moving on!")
+            break
+    if inSameGroup == False:
+        lowHalf = computeStep(start, int(length/2), data, location)
+        highHalf = computeStep(int(start+length/2), int(length/2), data, location)
+        answer2 = min(lowHalf, highHalf, answer2)
+    #print("Start:", start, "length", length, [startAnswer, endAnswer])
+    
+    return answer2
+
+
+answer1, answer2, i, j, game = 0,0,1,0, 1
+
+seeds = []
+soil = []
+fertilizer = []
+water = []
+light = []
+temperature = []
+humidity = []
+
+with open('input.txt', 'r') as file:
+    lines = file.readlines()
+
+# Extract seeds data
+seeds_line = lines[0].strip().split(': ')[1]
+seeds = list(map(int, seeds_line.split()))
+
+# Extract soil data
+soil_line_start = lines.index('seed-to-soil map:\n') + 1
+soil_line_end = lines.index('\n', soil_line_start)
+soil_lines = lines[soil_line_start:soil_line_end]
+soil = [list(map(int, line.split())) for line in soil_lines]
+
+# Extract fertilizer data
+fertilizer_line_start = lines.index('soil-to-fertilizer map:\n') + 1
+fertilizer_line_end = lines.index('\n', fertilizer_line_start)
+fertilizer_lines = lines[fertilizer_line_start:fertilizer_line_end]
+fertilizer = [list(map(int, line.split())) for line in fertilizer_lines]
+
+# Extract water data
+water_line_start = lines.index('fertilizer-to-water map:\n') + 1
+water_line_end = lines.index('\n', water_line_start)
+water_lines = lines[water_line_start:water_line_end]
+water = [list(map(int, line.split())) for line in water_lines]
+
+# Extract light data
+light_line_start = lines.index('water-to-light map:\n') + 1
+light_line_end = lines.index('\n', light_line_start)
+light_lines = lines[light_line_start:light_line_end]
+light = [list(map(int, line.split())) for line in light_lines]
+
+# Extract temperature data
+temperature_line_start = lines.index('light-to-temperature map:\n') + 1
+temperature_line_end = lines.index('\n', temperature_line_start)
+temperature_lines = lines[temperature_line_start:temperature_line_end]
+temperature = [list(map(int, line.split())) for line in temperature_lines]
+
+# Extract humidity data
+humidity_line_start = lines.index('temperature-to-humidity map:\n') + 1
+humidity_line_end = lines.index('\n', humidity_line_start)
+humidity_lines = lines[humidity_line_start:humidity_line_end]
+humidity = [list(map(int, line.split())) for line in humidity_lines]
+
+# Extract location data
+humidity_line_start = lines.index('humidity-to-location map:\n') + 1
+humidity_line_end = lines.index('\n', humidity_line_start)
+humidity_lines = lines[humidity_line_start:humidity_line_end]
+location = [list(map(int, line.split())) for line in humidity_lines]
+
+locationMap = 1000000000000
+data = []
+data.append(soil)
+data.append(fertilizer)
+data.append(water)
+data.append(light)
+data.append(temperature)
+data.append(humidity)
+data.append(location)
+
+#print(data)
+seedsPart2 = seeds.copy()
+answer2 = 10000000000000000000000000000
+for i in range(0, len(seedsPart2), 2):
+    answer2 = min(computeStep(seedsPart2[i], seedsPart2[i+1], data, location), answer2)
+    print(i/len(seedsPart2)*100,"%")
+
+answer1 = getAnswer1(data, seeds)
+
+#answer2 = getAnswer(data, seedsPart2)
+
+print("Part 1 Answer: ", answer1)
+print("Part 2 Answer: ", answer2)
+
+
+
---
+++ 2023/Day7_camel/2023_day7.py
@@ -0,0 +1,99 @@
+import math
+import re
+answer1, answer2, i, j, game = 0,0,1,0, 1
+
+games=[[],[],[],[],[],[],[]]
+class Game:
+    hand = []
+    bet = 0
+    tier = 0
+    rank = 0
+    value = 0
+    winnings = 0
+    hexHand = 0
+    originalHand = 0
+
+# def findGreater(hand1, hand2, pos):#assumes they are in the same tier    
+#     if((hand1 & (0<<pos)) == hand2 &(0<<pos)):
+#         return findGreater(hand1, hand2, pos-1)
+#     elif((hand1 & (0<<pos)) > hand2 &(0<<pos)):
+#         return True
+#     else: return False
+
+f = open("input.txt", "r")
+for i, line in enumerate(f):#read the input file
+    game = Game()
+    line = line.strip()
+    line = line.split(" ")
+    game.hand = list(line[0])
+    game.originalHand = list(line[0])
+    #convert face cards to hex numbers
+    for j, card in enumerate(game.hand):
+        if card == 'T':
+            game.hand[j] = 'A'
+        elif card == 'J':
+            game.hand[j] = '1'#swap this to 'B' to run part 1
+        elif card == 'Q':
+            game.hand[j] = 'C'
+        elif card == 'K':
+             game.hand[j] = 'D'
+        elif card == 'A':
+            game.hand[j] = 'E'
+    game.bet =(int(line[1]))
+    tempSet = set(game.hand)   
+    if(len(tempSet) == 5):#why does python not have switch statements??
+        if '1' in tempSet:
+            game.tier = 1#joker makes a pair, and there wasn't already a pair of jokers here
+        else: game.tier = 0#high card
+    elif(len(tempSet)==4): 
+            if '1' in tempSet:
+                game.tier = 3 #joker turns the pair into three of a kind (or two jokers merge with another card)
+            else: game.tier = 1#pair
+    elif(len(tempSet)==3): #three of a kind or two pair
+        isTwoPair = False
+        for card in game.hand:
+            if (game.hand.count(card) == 2):
+                isTwoPair = True
+                break
+        if(isTwoPair):
+            if '1' in tempSet:
+                if(game.hand.count('1') == 1):
+                    game.tier = 4 #two pair goes to full house (example: 22J33 becomes 22233)
+                else: game.tier = 5 #there must be 2 jokers, so two pair goes to four of a kind (example: 223JJ becomes 22322)
+            else: game.tier = 2
+        else: 
+            if '1' in tempSet:
+                game.tier = 5 #joker turns this to 4 of a kind
+            else: game.tier = 3 #three of a kind
+    elif(len(tempSet)==2): 
+        if(game.hand.count(game.hand[0]) == 4 or game.hand.count(game.hand[0]) == 1):#how many of the first character do we have?
+            if '1' in tempSet:
+                game.tier = 6 #joker turns this to 5 of a kind
+            else:   game.tier = 5#four of a kind
+        else: 
+            if '1' in tempSet:
+                game.tier = 6 # joker turns full house into 5 of a kind
+            else: game.tier =  4#full house
+    else:
+            game.tier = 6#five of a kind
+    game.hexHand= int(("".join(game.hand)), 16)#reverse the hand then convert to a hex number
+    games[game.tier].append(game)#make 7 different lists broken out by tier
+    print(game.tier)
+currentRank = 0
+
+for i, tierGroup in enumerate(games):
+    games[i] = sorted(games[i], key=lambda x: x.hexHand, reverse = False)
+    for j, round in enumerate(tierGroup):
+        currentRank += 1
+        games[i][j].rank = currentRank #only recording this for debugging purposes
+        games[i][j].winnings = currentRank * games[i][j].bet
+        answer1 += games[i][j].winnings
+        
+        print(games[i][j].hexHand, "".join(games[i][j].originalHand), currentRank, games[i][j].tier)
+   
+
+print("Part 1 Answer: ", answer1)
+#don't feel like making this work for part 1 and part 2
+#print("Part 2 Answer: ", answer2)
+
+
