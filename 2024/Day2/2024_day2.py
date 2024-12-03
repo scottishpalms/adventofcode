0a4bee27d46c5bbcb31d914c3aa1e7ac5c3d9794
Derek <dwc763@gmail.com>
2024-12-03 18:07:50 -0500

    day 2 done

--- 2024/Day2/2024_day2.py
+++ 2024/Day2/2024_day2.py
@@ -9,20 +9,33 @@ else:
      f = open(DAY_NAME + "/input.txt", "r").readlines()
 
 #part1:
-list1 = []
-list2 = []
+def isLineSafe(report):
+    increasing, decreasing, unsafe = False, False, False
+    for num, nextNum in zip(report, report[1:]):
+        if(nextNum < num):
+            decreasing = True
+        elif(nextNum > num):
+            increasing = True
+        else: 
+            unsafe = True
+        if((nextNum == num) or abs(nextNum - num) > 3):
+            unsafe = True
+    if(not(increasing and decreasing or unsafe)):
+        return True
+    else: return False
+
+
 for i, line in enumerate(f):
-    leftNum = int((line.split()[0]))
-    rightNum= int((line.split()[1]))
-    bisect.insort(list1, leftNum)
-    bisect.insort(list2, rightNum)
-    
-for i, item in enumerate(list1):
-    answer1 += (abs(list1[i] - list2[i]))
-    occurances = list2.count(item)
-    answer2 += item * occurances
-#part 2:
-      
+    report = list(map(int, line.split()))
+    if(isLineSafe(report)):
+       answer1+= 1
+       answer2+=1
+    else:
+        for j, level in enumerate(report):
+            dampenedReport = report[:j] + report[j+1:]#make a new list without one of the elements
+            if(isLineSafe(dampenedReport)):#this is slower than figuring out what the bad entry is specifically, but it works
+                answer2+=1
+                break
 
 print("Part 1 Answer: ", answer1)
 print("Part 2 Answer: ", answer2)
