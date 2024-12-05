8529f9bfe1fa262561beaf3928869a649e2c2c60
Derek <dwc763@gmail.com>
2024-12-05 00:18:35 -0500

    day4part2

--- 2024/Day4/2024_day4.py
+++ 2024/Day4/2024_day4.py
@@ -8,14 +8,13 @@ if(USE_TEST_INPUT):
 else:
      f = open(DAY_NAME + "/input.txt", "r").readlines()
 
-#part1
-pattern = r'(?=(XMAS|SAMX))'
+#part1: find all cases of "XMAS", horizontal, vertical, and diagonal (forwards and backwards)
 firstTime = True
 horizontal, vertical, diagL, diagR = [], [], [''], ['']
 horizontalString = ""
 width = 0
 startingIndex = 0
-
+#extract horizontal, vertical both diagonal strings:
 for lineNumber, line in enumerate(f):
     horizontal.append(line.strip())#the easy one
     width = len(line.strip())
@@ -43,16 +42,33 @@ for lineNumber, line in enumerate(f):
             diagR[lineNumber + i] += letter
         vertical[i] += letter    
     firstTime = False
+#now put all the strings together and find all the matches
 combinedList = [horizontal, vertical, diagL, diagR]
+pattern = r'(?=(XMAS|SAMX))'
 for direction in combinedList:
     answer1 +=  sum([len(re.findall(pattern, xmas)) for xmas in direction])
 
+#part2
+#you can't take my regex from me
+msPattern = r'M.S'
+smPattern = r'S.M'
+mmPattern = r'S.S'
+ssPattern = r'M.M'
+aPattern  = r'.A.'
+patterns = [
+    (ssPattern, aPattern, mmPattern),  # Match SS_MM
+    (mmPattern, aPattern, ssPattern),  # Match MM_SS
+    (msPattern, aPattern, msPattern),  # Match MS_MS
+    (smPattern, aPattern, smPattern),  # Match SM_SM
+]
 
-print (horizontal)
-print (vertical)
-print(diagL)
-print(diagR)
-          
+for i in range(width-2):
+    for j in range(width-2): #input is a "square"
+        for pattern1, pattern2, pattern3 in patterns:
+            if (re.match(pattern1, horizontal[i][j:j+3]) and
+                re.match(pattern2, horizontal[i+1][j:j+3]) and
+                re.match(pattern3, horizontal[i+2][j:j+3])):
+                answer2 += 1
+
 print("Part 1 Answer: ", answer1)
 print("Part 2 Answer: ", answer2)
-
